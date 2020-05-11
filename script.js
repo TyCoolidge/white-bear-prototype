c9d0f54b2e79703a19d3fdffb68bf50945c25984
TyCoolidge <62319016+TyCoolidge@users.noreply.github.com>
2020-05-10 22:24:44 -0700

    Create edit page and added working jquery

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-$("#save-imagery").click(function () {
-   $("#overlay-success").toggleClass("d-flex d-none");
-});
+$(document).ready(function () {
+   $("#save-imagery").click(function () {
+      $("#overlay-success").toggleClass("d-flex d-none");
+   });
 
-$("#create-error").click(function () {
-   $("#overlay-danger").toggleClass("d-flex d-none");
+   $("#create-error").click(function () {
+      $("#overlay-danger").toggleClass("d-flex d-none");
+   });
+
+   // $("#dropdownMenuButton").click(function () {
+   //    $("#dropdownMenuButton").dropdown("dropdown-menu");
+   // });
+
+   $("#deleteCard").hide(),
+      $("#showButton").click(function () {
+         $("#deleteCard").toggle("#deleteCard");
+      });
 });
