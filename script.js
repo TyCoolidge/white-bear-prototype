e6f3e9a24e749c33adaf8f65c70a4b9f312b17ef
TyCoolidge <62319016+TyCoolidge@users.noreply.github.com>
2020-05-11 17:17:06 -0700

    fixed edit card margins, add color to checkbox

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
    // });
 
    $("#deleteCard").hide(),
-      $("#showButton").click(function () {
+      $("#customCheck").click(function () {
          $("#deleteCard").toggle("#deleteCard");
       });
 
