3a3a6a577daf4918b5e65749ccf9fd553d76b18d
TyCoolidge <62319016+TyCoolidge@users.noreply.github.com>
2020-05-15 12:45:19 -0700

    added email/password validation to "welcome back"

--- script.js
+++ script.js
@@ -26,15 +26,67 @@ $("#signup").click(function () {
 
 //create if statement that displays red error message if email and password are blank
 $("#letsGo").click(function () {
-   var emailInput = $("#email1").val();
+   var emailInput = $("#emailRequired").val();
    if (emailInput.length === 0) {
       //if userinput equals, then when clicking lets go , error lights up
       $("#warningEmail").show();
-      $("#email1").addClass("is-invalid");
+      $("#emailRequired").addClass("is-invalid");
    } else {
-      $("#email1").removeClass("is-invalid");
-      $("#email1").addClass("is-valid");
+      $("#emailRequired").removeClass("is-invalid");
+      $("#emailRequired").addClass("is-valid");
       $("#warningEmail").hide();
       // console.log("warningEmail"); console.log for the dev's eyes/ can not call ID
+   }
+});
+
+// if lets go clicked and password empty, display first error and light up red
+$("#letsGo").click(function () {
+   var validPassword = $("#requiredPassword").val();
+   if (validPassword.length === 0) {
+      $("#warningPassword").show();
+      $("#requiredPassword").addClass("is-invalid");
+      // else if password less than 9 characters, display character error and also light up
+   } else if (validPassword.length <= 9) {
+      $("#warningPasswordCharacters").show();
+      $("#warningPassword").hide();
+      $("#requiredPassword").addClass("is-invalid");
+      // else remove invalid classes and hide error message
+   } else {
+      $("#requiredPassword").removeClass("is-invalid");
+      $("#requiredPassword").addClass("is-valid");
+      $("#warningPasswordCharacters, #warningPassword").hide();
+   }
+});
+
+//click log in with empty email will disable button and trigger error
+$("#logIn").click(function () {
+   var emailWelcomeInput = $("#welcomeBackEmail").val();
+   if (emailWelcomeInput.length === 0) {
+      $("#warningWelcomeEmail").show();
+      $("#welcomeBackEmail").addClass("is-invalid");
+      $("#logIn").removeAttr("href");
+   } else {
+      $("#welcomeBackEmail").removeClass("is-invalid");
+      $("#welcomeBackEmail").addClass("is-valid");
+      $("#warningWelcomeEmail").hide();
+   }
+});
+//need password and email filled out in order to proceed
+$("#logIn").click(function () {
+   var validWelcomePassword = $("#welcomePassword").val();
+   if (validWelcomePassword.length === 0) {
+      $("#warningWelcomePassword").show();
+      $("#welcomePassword").addClass("is-invalid");
+      // else if password less than 9 characters, display character error and also light up
+   } else if (validWelcomePassword.length <= 9) {
+      $("#warningWelcomePasswordCharacters").show();
+      $("#warningWelcomePassword").hide();
+      $("#welcomePassword").addClass("is-invalid");
+      // else remove invalid classes and hide error message
+   } else {
+      $("#welcomePassword").removeClass("is-invalid");
+      $("#welcomePassword").addClass("is-valid");
+      $("#warningWelcomePasswordCharacters, #warningWelcomePassword").hide();
+      $("#logIn").addAttr("href");
    }
 });
