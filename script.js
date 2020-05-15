e97555f1bf910fd3596dbea2bb3add2400d6c7ac
TyCoolidge <62319016+TyCoolidge@users.noreply.github.com>
2020-05-14 17:04:19 -0700

    add javascript to index page

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-$(document).ready(function () {
-   $("#save-imagery").click(function () {
-      $("#overlay-success").toggleClass("d-flex d-none");
+$("#save-imagery").click(function () {
+   $("#overlay-success").toggleClass("d-flex d-none");
+});
+
+$("#create-error").click(function () {
+   $("#overlay-danger").toggleClass("d-flex d-none");
+});
+
+// $("#dropdownMenuButton").click(function () {
+//    $("#dropdownMenuButton").dropdown("dropdown-menu");
+// });
+
+$("#deleteCard").hide(),
+   $("#customCheck").click(function () {
+      $("#deleteCard").toggle("#deleteCard");
    });
 
-   $("#create-error").click(function () {
-      $("#overlay-danger").toggleClass("d-flex d-none");
-   });
-
-   // $("#dropdownMenuButton").click(function () {
-   //    $("#dropdownMenuButton").dropdown("dropdown-menu");
-   // });
-
-   $("#deleteCard").hide(),
-      $("#customCheck").click(function () {
-         $("#deleteCard").toggle("#deleteCard");
-      });
-
-   $("#dragdown, #signUpText").hide(),
-      $("#signup").click(function () {
-         $("#dragdown, #signUpText").slideDown("#dragdown, #signUpText");
-      });
-
+$("#dragdown, #signUpText").hide(),
    $("#signup").click(function () {
-      $("#signup").hide();
+      $("#dragdown, #signUpText").slideDown("#dragdown, #signUpText");
    });
+
+$("#signup").click(function () {
+   $("#signup").hide();
+});
+
+//create if statement that displays red error message if email and password are blank
+$("#letsGo").click(function () {
+   var emailInput = $("#email1").val();
+   if (emailInput.length === 0) {
+      //if userinput equals, then when clicking lets go , error lights up
+      $("#warningEmail").show();
+      $("#email1").addClass("is-invalid");
+   } else {
+      $("#email1").removeClass("is-invalid");
+      $("#email1").addClass("is-valid");
+      $("#warningEmail").hide();
+      // console.log("warningEmail"); console.log for the dev's eyes/ can not call ID
+   }
 });
